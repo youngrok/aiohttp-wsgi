933dd0f67785d910352d10827ff8773f0002194d
Pak Youngrok <pak.youngrok@gmail.com>
2016-04-26 16:45:39 +0900

    pass loop argument manually because it's not in kwargs

--- aiohttp_wsgi/api.py
+++ aiohttp_wsgi/api.py
@@ -61,7 +61,7 @@ def configure_server(application, *,
     if not script_name.startswith("/"):  # pragma: no cover
         raise ValueError("Script name should start with /")
     # Add the wsgi application. This has to be last.
-    app.router.add_route("*", "{}{{path_info:.*}}".format(script_name), WSGIHandler(application, **kwargs).handle_request)
+    app.router.add_route("*", "{}{{path_info:.*}}".format(script_name), WSGIHandler(application, loop=loop, **kwargs).handle_request)
     # Set up the server.
     shared_server_kwargs = {
         "backlog": backlog,
